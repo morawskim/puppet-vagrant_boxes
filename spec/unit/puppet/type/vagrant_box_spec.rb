6503565b2d03fe7f31ca0182ccf26bae1ca1527e
Marcin Morawski <marcin@morawskim.pl>
2017-08-03 10:09:37 +0200

    rspec vagrant_boxes type

--- spec/unit/puppet/type/vagrant_box_spec.rb
+++ spec/unit/puppet/type/vagrant_box_spec.rb
@@ -1,7 +1,23 @@
 #! /usr/bin/env ruby
 
 describe Puppet::Type.type(:vagrant_box) do
-    it "test" do
-        expect(true).to_eq "aaa"
-    end
+
+  before :each do
+    @vagrant_box = Puppet::Type.type(:vagrant_box).new(
+    :name => 'opensuse/openSUSE-13.2-x86_64',
+      :version => '1.0.0',
+    )
+  end
+
+  it 'should accept name' do
+    expect(@vagrant_box[:name]).to eq('opensuse/openSUSE-13.2-x86_64')
+  end
+
+  it 'should set default box_provider to virtualbox' do
+    expect(@vagrant_box[:box_provider]).to eq('virtualbox')
+  end
+
+  it 'should set version' do
+    expect(@vagrant_box[:version]).to eq('1.0.0')
+  end
 end
